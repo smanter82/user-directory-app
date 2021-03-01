d0ea21280016527dc8968989b64fb4462cd8bcf5
Sarah <70615195+smanter82@users.noreply.github.com>
2021-03-01 15:53:24 -0500

    committing unsaved file

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,55 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "../styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Select from "react-select";
 
-function Table(props) {
-  console.log(props.users);
+function Table() {
+  const [users, setUser] = useState({
+    employees: [],
+    filteredEmployees: [],
+  });
+  useEffect(() => {
+    const data = fetch("https://randomuser.me/api/?results=5");
+    data
+      .then((response) => response.json())
+      .then((response) => setUser({ employees: response.results }));
+  }, []);
 
+  // const handleFilterClick = () => {
+  //   console.log("clicked");
+
+  // };
+
+  const handleInputChange = () => {
+    console.log("input changed");
+    //define state, update state, grab state from button click
+    const arr = [users.employees];
+    // console.log(arr);
+    const filteredArr = arr.filter(
+      () => event.target.value === users.employees.email
+    );
+    console.log(event.target.value);
+    setUser({ filteredEmployees: filteredArr });
+  };
+
+  const handleSortInputChange = () => {
+    console.log("sort input changed");
+  };
+
+  const options = [
+    { value: "First Name", label: "Last Name" },
+    { value: "Last Name", label: "First Name" },
+  ];
   return (
     <div className="App">
       <h1>Employee Info Table</h1>
       <input
         type="text"
+        name="filterInput"
         placeholder="Filter results by"
-        onChange={props.handleInputChange}
+        onChange={handleInputChange}
       />
-      <button
-        type="button"
-        onClick={props.handleFilterClick}
-        className="btn btn-primary"
-      >
-        Filter
-      </button>
+      <Select options={options} />
       <table className="table">
         <thead>
           <tr>
@@ -32,7 +62,7 @@ function Table(props) {
           </tr>
         </thead>
         <tbody>
-          {props.users.employees.map((employee, index) => (
+          {users.employees.map((employee, index) => (
             <tr key={index}>
               <td>{employee.name.first}</td>
               <td>{employee.name.last}</td>
